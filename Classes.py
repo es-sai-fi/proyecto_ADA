69558f114eb68fbab3e7cfcf3ee97ba037cb7a8b
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-07-14 16:52:40 -0500

    initialize

---
+++ Classes.py
@@ -0,0 +1,57 @@
+class ParticipantArray:
+  def __init__(self, elements):
+    self.elements = elements
+    
+  def getNumOfParticipants():
+    pass
+  
+  def getOpinionMean():
+    pass
+  
+  def getExperticeLevelMean():
+    pass
+  
+class QuestionArray:
+  def __init__(self, elements):
+    self.elements = elements
+    
+  def getNumOfParticipants():
+    pass
+  
+  def getOpinionMean():
+    pass
+  
+  def getExperticeLevelMean():
+    pass
+
+class Tree:
+  def __init__(self, root, nodes):
+    self.root = root
+    self.nodes = nodes
+
+class Node:
+  def __init__(self, parent, left, right, key):
+    self.parent = parent
+    self.left = left
+    self.right = right
+    self.key = key
+
+class Participant:
+  def __init__(self, name, expertiseLevel, opinion):
+    self.name = name
+    self.expertiseLevel = expertiseLevel
+    self.opinion = opinion
+    
+class Question:
+  def __init__(self, participants):
+    self.participants = participants
+    self.participantsSize = participants.getNumOfParticipants()
+    self.opinionMean = participants.getOpinionMean()
+    self.expertiseLevelMean = participants.getExpertiseLevelMean()
+  
+class Topic:
+  def __init__(self, questions):
+    self.questions = questions
+    self.participants = questions.getNumOfParticipants()
+    self.opinionMeanOfMeans = questions.getOpinionMean()
+    self.expertiseLevelMeanOfMeans = questions.getExpertiseLevelMean()
