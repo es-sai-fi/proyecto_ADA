45f0ac7094d9cc20b5ced1eeb5bd73e8dac238f4
Isabella Ruiz <ruiz.isabella@correounivalle.edu.co>
2025-07-25 00:15:53 -0500

    Tests

--- core/generator.py
+++ core/generator.py
@@ -48,13 +48,69 @@ def formatOutput(participants, questionBlocks):
       
   return "".join(lines)
   
-# Parámetros
-K = 2 # Número de temas
-M = 2 # Número de preguntas por tema
-Nmin = 3 # Mínimo número de encuestados por pregunta
-Nmax = 5 # Máximo número de encuestados por pregunta
+# Parámetros test 4 (Muchos temas)
+K = 10 # Número de temas
+M = 50 # Número de preguntas por tema
+Nmin = 1 # Mínimo número de encuestados por pregunta
+Nmax = 3 # Máximo número de encuestados por pregunta
 mode = "random" # Modo de generación
 path = "tests/Test4.txt" # Ruta donde guardar el output
+
+participants, questionBlocks = generateData(K, M, Nmin, Nmax, mode)
+formattedText = formatOutput(participants, questionBlocks)
+
+with open(path, "w", encoding="utf-8") as f:
+  f.write(formattedText)
+
+# Parámetros test 5 (Mismo K, mayor M)
+K = 10 # Número de temas
+M = 100 # Número de preguntas por tema
+Nmin = 1 # Mínimo número de encuestados por pregunta
+Nmax = 3 # Máximo número de encuestados por pregunta
+mode = "random" # Modo de generación
+path = "tests/Test5.txt" # Ruta donde guardar el output
+
+participants, questionBlocks = generateData(K, M, Nmin, Nmax, mode)
+formattedText = formatOutput(participants, questionBlocks)
+
+with open(path, "w", encoding="utf-8") as f:
+  f.write(formattedText)
+# Parámetros test 6 (Numeros Grandes)
+K = 100 # Número de temas
+M = 50 # Número de preguntas por tema
+Nmin = 10 # Mínimo número de encuestados por pregunta
+Nmax = 30 # Máximo número de encuestados por pregunta
+mode = "random" # Modo de generación
+path = "tests/Test6.txt" # Ruta donde guardar el output
+
+participants, questionBlocks = generateData(K, M, Nmin, Nmax, mode)
+formattedText = formatOutput(participants, questionBlocks)
+
+with open(path, "w", encoding="utf-8") as f:
+  f.write(formattedText)
+
+# Parámetros test 7 (Pocos temas con alto N)
+K = 5 # Número de temas
+M = 5 # Número de preguntas por tema
+Nmin = 100 # Mínimo número de encuestados por pregunta
+Nmax = 150 # Máximo número de encuestados por pregunta
+mode = "random" # Modo de generación
+path = "tests/Test7.txt" # Ruta donde guardar el output
+
+participants, questionBlocks = generateData(K, M, Nmin, Nmax, mode)
+formattedText = formatOutput(participants, questionBlocks)
+
+with open(path, "w", encoding="utf-8") as f:
+  f.write(formattedText)
+
+# Parámetros test 8 (Muchos temas,muchos estudiantes)
+K = 200 # Número de temas
+M = 100 # Número de preguntas por tema
+Nmin = 20 # Mínimo número de encuestados por pregunta
+Nmax = 50 # Máximo número de encuestados por pregunta
+mode = "random" # Modo de generación
+path = "tests/Test4.txt" # Ruta donde guardar el output
+
 
 participants, questionBlocks = generateData(K, M, Nmin, Nmax, mode)
 formattedText = formatOutput(participants, questionBlocks)
